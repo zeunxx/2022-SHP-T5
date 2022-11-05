f62b25c26ac2906f0aab447a9b43495e675655e5
zeun <real_jieun99@daum.net>
2022-11-05 23:50:15 +0900

    server 셋팅

---
+++ server/newknews/src/main/java/shp_t5/newknews/NewknewsApplication.java
@@ -0,0 +1,13 @@
+package shp_t5.newknews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewknewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewknewsApplication.class, args);
+	}
+
+}
